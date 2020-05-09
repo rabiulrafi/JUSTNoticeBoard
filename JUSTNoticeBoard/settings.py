4dfdc12d01ba485a8aa3ec909d05b62136783a8c
rabiulrafi <rabiulrafi01@gmail.com>
2020-05-09 11:26:35 +0600

    staticfiles

--- JUSTNoticeBoard/settings.py
+++ JUSTNoticeBoard/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ii$q1r@+=3i^fa3z0h=*advsw8ms__dvw-(fjhnq_vxq3xs87g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['just-notice-board.herokuapp.com','127.0.0.1']
 
